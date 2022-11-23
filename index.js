963fc137a4667b9614c8bcbd271d5cd7e253c439
Tom Hestamp <tom@hestamp.me>
2022-11-23 02:26:01 +0200

    fix

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get('/auth/me', checkAuth, UserControllers.getMe)
 
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
   res.json({
-    url: `/uploads/${req.file.originalname}`,
+    url: `uploads/${req.file.originalname}`,
   })
 })
 
