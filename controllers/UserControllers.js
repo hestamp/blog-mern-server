640fbeba84d4f6522d127b267736edc9c33dc396
Tom Hestamp <tom@hestamp.me>
2022-11-26 13:34:38 +0200

    secretFix

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -21,7 +21,7 @@ export const register = async (req, res) => {
       {
         _id: user._id,
       },
-      'secret123',
+      process.env.JWT_SECRET,
       {
         expiresIn: '30d',
       }
@@ -66,7 +66,7 @@ export const login = async (req, res) => {
       {
         _id: user._id,
       },
-      'secret123',
+      process.env.JWT_SECRET,
       {
         expiresIn: '30d',
       }
