1ae9cf550443f63de59441d36fbb6d898b565891
Tom Hestamp <tom@hestamp.me>
2022-11-26 20:26:59 +0200

    fix

--- models/Post.js
+++ models/Post.js
@@ -21,7 +21,7 @@ const PostSchema = new mongoose.Schema(
     },
     ofDates: [Date],
     user: {
-      type: mongoose.Schema.Types.ObjectId.user._id,
+      type: mongoose.Schema.Types.ObjectId._id,
       ref: 'User',
       required: true,
     },
