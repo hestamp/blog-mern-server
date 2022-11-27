7a2cc2deecad4fba56297f2ec9863405c58e6a3c
Tom Hestamp <tom@hestamp.me>
2022-11-27 23:40:09 +0200

    no tags

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -94,7 +94,6 @@ export const create = async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
       user: req.userId,
     })
 
@@ -122,7 +121,6 @@ export const update = async (req, res) => {
         text: req.body.text,
         imageUrl: req.body.imageUrl,
         user: req.userId,
-        tags: req.body.tags,
       }
     )
 
